d114aad03540657720f8570ae543d56071dd3d95
Alejandro Carvajal <ale.carva16@hotmail.com>
2024-11-06 20:21:09 -0600

    Working on animation with kute.js svg Draw

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+const startDrawAnimations = () => {
+    var tween1 = KUTE.fromTo('capa',{draw:'0% 10%'}, {draw:'90% 100%'});
+  };
+
+  // Llama a la función para iniciar las animaciones
+  startDrawAnimations();
